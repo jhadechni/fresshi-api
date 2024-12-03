2c4d8874dba73c41c1c628f2f8dc345e60c95a0d
jaime.sierra <jaime.sierra@banistmo.com>
2024-12-02 23:02:12 -0500

    update Firebase configuration to use base64 encoded credentials and add .env to .gitignore

--- src/infrastructure/firebase/firebaseConfig.ts
+++ src/infrastructure/firebase/firebaseConfig.ts
@@ -3,8 +3,12 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
+const base64Credentials = process.env.FIREBASE_CREDENTIALS_BASE64;
+if (!base64Credentials) {
+  throw new Error('FIREBASE_CREDENTIALS_BASE64 no está definida');
+}
 
-const serviceAccount = require('../../../firebaseCredentials.json');
+const serviceAccount = JSON.parse(Buffer.from(base64Credentials, 'base64').toString('utf-8'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
