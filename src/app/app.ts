22abb5940f8a5fd835701d03e514da475b848e80
jaime.sierra <jaime.sierra@banistmo.com>
2024-12-16 23:50:04 -0500

    add CORS support and update server port to 4000

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import dataRoutes from '../adapters/routes/dataRoutes';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
+// Configurar CORS para permitir cualquier origen
+app.use(cors());
 
 app.use(express.json());
 app.use('/api/data', dataRoutes);
